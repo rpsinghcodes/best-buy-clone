2485c32e8781039208d342ae3ceb0b56e692b250
Sameeer Ahmad Mir <meersameer305@gmail.com>
2024-12-21 22:41:09 -0800

    Completed Implementation of product page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-background":
+          "url('https://pisces.bbystatic.com/image2/BestBuy_US/dam/REF-1956487-ghp-lms-241219-28c61967-5cd9-4a71-a201-49583c903c40.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
